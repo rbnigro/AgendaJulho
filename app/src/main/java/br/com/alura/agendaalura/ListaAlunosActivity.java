22e26014b7aa274b7aba685c51d76b6badd6d044
Ronney Nigro <ronneynigro@gmail.com>
2018-08-05 13:15:11 -0300

    Aula 5

--- app/src/main/java/br/com/alura/agendaalura/ListaAlunosActivity.java
+++ app/src/main/java/br/com/alura/agendaalura/ListaAlunosActivity.java
@@ -1,9 +1,12 @@
 package br.com.alura.agendaalura;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.Browser;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -78,16 +81,38 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     @Override
     public void onCreateContextMenu(ContextMenu menu, View v, final ContextMenu.ContextMenuInfo menuInfo) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        final Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
+
+        MenuItem itemSMS = menu.add("Enviar SMS");
+        Intent inteSMS = new Intent(Intent.ACTION_VIEW);
+        inteSMS.setData(Uri.parse("sms:" + aluno.getTelefone()));
+        itemSMS.setIntent(inteSMS);
+
+        MenuItem itemMapa = menu.add("Visualizar no Mapa");
+        Intent intentMapa = new Intent(Intent.ACTION_VIEW);
+        intentMapa.setData(Uri.parse("geo:0,0?q=" + aluno.getEndereco()));
+        itemMapa.setIntent(intentMapa);
+
+        MenuItem itemSite = menu.add("Visitar Site");
+        Intent intentSite = new Intent(Intent.ACTION_VIEW);
+
+        String site = aluno.getSite();
+        if (!site.startsWith("http://")) {
+            site = "http://" + site;
+        }
+
+        intentSite.setData(Uri.parse(site));
+        itemSite.setIntent(intentSite);
+
         MenuItem deletar = menu.add("Deletar");
         deletar.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
-                AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
-                Aluno aluno = (Aluno) listaAlunos.getItemAtPosition(info.position);
                 AlunoDAO alunoDAO  = new AlunoDAO(ListaAlunosActivity.this);
                 alunoDAO.deleta(aluno);
                 alunoDAO.close();
-                // Toast.makeText(ListaAlunosActivity.this, "Aluno: " + aluno.getId(), Toast.LENGTH_SHORT).show();
+
                 carregaLista();
                 return false;
             }
